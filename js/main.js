8203135a0a434923d69e806da8c16f44acd7d5fb
gigabittu <gigabittu@gmail.com>
2026-08-09 04:03:42 +0530

    Update Group Entities slider to show 1 card per view on tablet (same as mobile)

--- js/main.js
+++ js/main.js
@@ -229,7 +229,6 @@ function initGroupCompaniesSlider() {
 
   function getVisibleCount() {
     if (window.innerWidth >= 1024) return 3;
-    if (window.innerWidth >= 768) return 2;
     return 1;
   }
 
