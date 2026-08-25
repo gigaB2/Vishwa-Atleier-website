b5e65c59e07373abafef3d4beee35751de2adeac
gigabittu <gigabittu@gmail.com>
2026-08-26 01:10:27 +0530

    feat: rename doubler costing calculator to Doubler/MX costing calculator across suite

--- management suite/sidebar.js
+++ management suite/sidebar.js
@@ -198,7 +198,7 @@
         </a>
         <a class="vf-sb-link child" data-mode="yarn" href="modules/yarn/yarn-costing.html?tab=doubler">
           <span class="vf-sb-icon vf-sb-icon-green vf-sb-icon-sm"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="1" x2="12" y2="23"/><path d="M17 5H9.5a3.5 3.5 0 000 7h5a3.5 3.5 0 010 7H6"/></svg></span>
-          Doubler Costing Calculator
+          Doubler/MX Costing Calculator
         </a>
         <a class="vf-sb-link child" data-mode="yarn" href="modules/yarn/yarn-costing.html?tab=covering">
           <span class="vf-sb-icon vf-sb-icon-green vf-sb-icon-sm"><svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="1" x2="12" y2="23"/><path d="M17 5H9.5a3.5 3.5 0 000 7h5a3.5 3.5 0 010 7H6"/></svg></span>
